74acf47acf70d02108cbdd356e233a52562b46d4
Philippe Vinchon <p.vinchon@gmail.com>
2020-05-23 11:57:52 +0100

    Recipes API with volatile storage

---
+++ app/models/Recipe.scala
@@ -0,0 +1,48 @@
+package models
+
+import java.time.LocalDateTime
+import java.util.UUID
+import play.api.libs.json.Json
+
+case class Recipe(
+  id: UUID,
+  name: String,
+  description: String,
+  createdAt: LocalDateTime,
+  updatedAt: LocalDateTime,
+)
+
+object Recipe {
+  implicit def recipePlayJsonWrites = Json.writes[Recipe]
+
+  def fromForm(form: RecipeForm): Recipe =
+    Recipe(
+      id = UUID.randomUUID,
+      name = form.name,
+      description = form.description,
+      createdAt = LocalDateTime.now(),
+      updatedAt = LocalDateTime.now(),
+    )
+
+  def updated(self: Recipe)(form: RecipeForm): Recipe =
+    self.copy(
+      name = form.name,
+      description = form.description,
+      updatedAt = LocalDateTime.now(),
+    )
+}
+
+case class RecipeForm(
+  name: String,
+  description: String,
+)
+
+object RecipeForm {
+  implicit def recipeFormPlayJsonReads = Json.reads[RecipeForm]
+
+  def fromModel(model: Recipe): RecipeForm =
+    RecipeForm(
+      name = model.name,
+      description = model.description,
+    )
+}
