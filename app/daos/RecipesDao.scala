6df5b3b440875e76bf672d41a9a0982c7e5ee995
Philippe Vinchon <p.vinchon@gmail.com>
2020-05-26 18:37:25 +0100

    Add package name

--- app/daos/RecipesDao.scala
+++ app/daos/RecipesDao.scala
@@ -9,70 +9,69 @@ import slick.jdbc.{JdbcProfile, PostgresProfile, GetResult}
 
 import helpers.slick.dbioaction._
 import helpers.slick.jdbc._
-import models.Recipe
 
 trait RecipesDao {
-  def findAll(): Future[Seq[Recipe]]
-  def findById(id: UUID): Future[Option[Recipe]]
-  def insert(recipe: Recipe): Future[Unit]
-  def update(recipe: Recipe): Future[Unit]
-  def delete(recipe: Recipe): Future[Unit]
+  def findAll(): Future[Seq[models.Recipe]]
+  def findById(id: UUID): Future[Option[models.Recipe]]
+  def insert(recipe: models.Recipe): Future[Unit]
+  def update(recipe: models.Recipe): Future[Unit]
+  def delete(recipe: models.Recipe): Future[Unit]
 }
 
 @Singleton
 class RecipesDaoMap extends RecipesDao {
   val map = collection.mutable.Map.empty[UUID, models.Recipe]
 
-  def findAll(): Future[Seq[Recipe]] = Future.successful(map.values.toSeq)
-  def findById(id: UUID): Future[Option[Recipe]] = Future.successful(map.get(id))
-  def insert(recipe: Recipe): Future[Unit] = Future.successful(map.update(recipe.id, recipe))
-  def update(recipe: Recipe): Future[Unit] = Future.successful(map.update(recipe.id, recipe))
-  def delete(recipe: Recipe): Future[Unit] = Future.successful(map.remove(recipe.id))
+  def findAll(): Future[Seq[models.Recipe]] = Future.successful(map.values.toSeq)
+  def findById(id: UUID): Future[Option[models.Recipe]] = Future.successful(map.get(id))
+  def insert(recipe: models.Recipe): Future[Unit] = Future.successful(map.update(recipe.id, recipe))
+  def update(recipe: models.Recipe): Future[Unit] = Future.successful(map.update(recipe.id, recipe))
+  def delete(recipe: models.Recipe): Future[Unit] = Future.successful(map.remove(recipe.id))
 }
 
 class RecipesDaoSlick @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[JdbcProfile] with RecipesDao {
   import profile.api._
 
   private val table = TableQuery[RecipesTable]
-  private class RecipesTable(tag: Tag) extends Table[Recipe](tag, "recipes") {
+  private class RecipesTable(tag: Tag) extends Table[models.Recipe](tag, "recipes") {
     def id = column[UUID]("id", O.PrimaryKey)
     def name = column[String]("name")
     def description = column[String]("description")
     def createdAt = column[LocalDateTime]("created_at")
     def updatedAt = column[LocalDateTime]("updated_at")
 
-    def * = (id, name, description, createdAt, updatedAt).mapTo[Recipe]
+    def * = (id, name, description, createdAt, updatedAt).mapTo[models.Recipe]
   }
 
-  def findAll(): Future[Seq[Recipe]] = db.run(table.result)
-  def findById(id: UUID): Future[Option[Recipe]] = db.run(table.filter(_.id === id).result.headOption)
-  def insert(recipe: Recipe): Future[Unit] = db.run((table += recipe).void)
-  def update(recipe: Recipe): Future[Unit] = db.run(table.filter(_.id === recipe.id).update(recipe).void)
-  def delete(recipe: Recipe): Future[Unit] = db.run(table.filter(_.id === recipe.id).delete.void)
+  def findAll(): Future[Seq[models.Recipe]] = db.run(table.result)
+  def findById(id: UUID): Future[Option[models.Recipe]] = db.run(table.filter(_.id === id).result.headOption)
+  def insert(recipe: models.Recipe): Future[Unit] = db.run((table += recipe).void)
+  def update(recipe: models.Recipe): Future[Unit] = db.run(table.filter(_.id === recipe.id).update(recipe).void)
+  def delete(recipe: models.Recipe): Future[Unit] = db.run(table.filter(_.id === recipe.id).delete.void)
 }
 
 class RecipesDaoSlickPlainSql @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends HasDatabaseConfigProvider[PostgresProfile] with RecipesDao {
   import profile.api._
 
-  implicit val getRecipeResult = GetResult(r => Recipe(r.<<, r.<<, r.<<, r.<<, r.<<))
+  implicit val getRecipeResult = GetResult(r => models.Recipe(r.<<, r.<<, r.<<, r.<<, r.<<))
 
-  def findAll(): Future[Seq[Recipe]] = db.run(sql"""
+  def findAll(): Future[Seq[models.Recipe]] = db.run(sql"""
     select id, name, description, created_at, updated_at
     from recipes
-  """.as[Recipe])
+  """.as[models.Recipe])
 
-  def findById(id: UUID): Future[Option[Recipe]] = db.run(sql"""
+  def findById(id: UUID): Future[Option[models.Recipe]] = db.run(sql"""
     select id, name, description, created_at, updated_at
     from recipes
     where id = ${id}
-  """.as[Recipe].headOption)
+  """.as[models.Recipe].headOption)
 
-  def insert(recipe: Recipe): Future[Unit] = db.run(sqlu"""
+  def insert(recipe: models.Recipe): Future[Unit] = db.run(sqlu"""
     insert into recipes(id, name, description, created_at, updated_at)
     values (${recipe.id}, ${recipe.name}, ${recipe.description}, ${recipe.createdAt}, ${recipe.updatedAt})
   """.void)
 
-  def update(recipe: Recipe): Future[Unit] = db.run(sqlu"""
+  def update(recipe: models.Recipe): Future[Unit] = db.run(sqlu"""
     update recipes
     set name = ${recipe.name},
       description = ${recipe.description},
@@ -81,7 +80,7 @@ class RecipesDaoSlickPlainSql @Inject()(protected val dbConfigProvider: Database
     where id = ${recipe.id}
   """.void)
 
-  def delete(recipe: Recipe): Future[Unit] = db.run(sqlu"""
+  def delete(recipe: models.Recipe): Future[Unit] = db.run(sqlu"""
     delete from recipes
     where id = ${recipe.id}
   """.void)
