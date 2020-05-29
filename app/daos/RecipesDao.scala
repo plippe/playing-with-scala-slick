2d9899c0b3d66e7db54fc7d27be6dc47115209aa
Philippe Vinchon <p.vinchon@gmail.com>
2020-05-29 16:25:22 +0100

    Remove helper void for simplicity

--- app/daos/RecipesDao.scala
+++ app/daos/RecipesDao.scala
@@ -5,10 +5,8 @@ import java.util.UUID
 import javax.inject.{Inject, Singleton}
 import play.api.db.slick._
 import scala.concurrent.Future
+import slick.dbio.DBIOAction
 import slick.jdbc.{JdbcProfile, GetResult}
-
-import helpers.slick.dbioaction._
-import helpers.slick.jdbc._
 
 trait RecipesDao {
   def findAll(): Future[Seq[models.Recipe]]
@@ -43,17 +41,22 @@ class RecipesDaoSlick @Inject()(protected val dbConfigProvider: DatabaseConfigPr
     def * = (id, name, description, createdAt, updatedAt).mapTo[models.Recipe]
   }
 
+  val void = DBIOAction.successful(())
+
   def findAll(): Future[Seq[models.Recipe]] = db.run(table.result)
   def findById(id: UUID): Future[Option[models.Recipe]] = db.run(table.filter(_.id === id).result.headOption)
-  def insert(recipe: models.Recipe): Future[Unit] = db.run((table += recipe).void)
-  def update(recipe: models.Recipe): Future[Unit] = db.run(table.filter(_.id === recipe.id).update(recipe).void)
-  def delete(recipe: models.Recipe): Future[Unit] = db.run(table.filter(_.id === recipe.id).delete.void)
+  def insert(recipe: models.Recipe): Future[Unit] = db.run((table += recipe).andThen(void))
+  def update(recipe: models.Recipe): Future[Unit] = db.run(table.filter(_.id === recipe.id).update(recipe).andThen(void))
+  def delete(recipe: models.Recipe): Future[Unit] = db.run(table.filter(_.id === recipe.id).delete.andThen(void))
 }
 
 class RecipesDaoSlickPlainSql @Inject()(protected val dbConfigProvider: DatabaseConfigProvider) extends RecipesDao with HasDatabaseConfigProvider[JdbcProfile] {
   import profile.api._
+  import helpers.slick.jdbc._
 
   implicit val getRecipeResult = GetResult(r => models.Recipe(r.<<, r.<<, r.<<, r.<<, r.<<))
+
+  val void = DBIOAction.successful(())
 
   def findAll(): Future[Seq[models.Recipe]] = db.run(sql"""
     select id, name, description, created_at, updated_at
@@ -69,7 +72,7 @@ class RecipesDaoSlickPlainSql @Inject()(protected val dbConfigProvider: Database
   def insert(recipe: models.Recipe): Future[Unit] = db.run(sqlu"""
     insert into recipes(id, name, description, created_at, updated_at)
     values (${recipe.id}, ${recipe.name}, ${recipe.description}, ${recipe.createdAt}, ${recipe.updatedAt})
-  """.void)
+  """.andThen(void))
 
   def update(recipe: models.Recipe): Future[Unit] = db.run(sqlu"""
     update recipes
@@ -78,10 +81,10 @@ class RecipesDaoSlickPlainSql @Inject()(protected val dbConfigProvider: Database
       created_at = ${recipe.createdAt},
       updated_at = ${recipe.updatedAt}
     where id = ${recipe.id}
-  """.void)
+  """.andThen(void))
 
   def delete(recipe: models.Recipe): Future[Unit] = db.run(sqlu"""
     delete from recipes
     where id = ${recipe.id}
-  """.void)
+  """.andThen(void))
 }
